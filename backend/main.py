375f44067975810e0bbc31d8176b570c904047bf
Vishal Rathod <1155defender@gmail.com>
2025-12-25 18:38:02 +0530

    Add FastAPI server and basic endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from fastapi import FastAPI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "FastAPI is working"}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.get("/users")
+def get_users():
+    return [
+        {"id": 1, "name": "Vishal"},
+        {"id": 2, "name": "Rahul"}
+    ]
